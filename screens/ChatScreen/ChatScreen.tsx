83408b7b71967085638e4ace85413e10f66c016a
Nickita <102178609+manihateu@users.noreply.github.com>
2024-11-26 20:57:33 +0300

    Update ChatScreen.tsx

--- screens/ChatScreen/ChatScreen.tsx
+++ screens/ChatScreen/ChatScreen.tsx
@@ -1,4 +1,4 @@
-import { SafeAreaView } from "react-native"
+import { SafeAreaView, StatusBar } from "react-native"
 import MessageBubble from "./components/MessageBubble"
 import { mockData } from "./mock.data"
 import { FlatList } from "react-native-gesture-handler"
@@ -8,7 +8,7 @@ export const ChatScreen = () => {
     
     return (
         
-        <SafeAreaView>
+        <SafeAreaView style={{marginTop: StatusBar.currentHeight}}>
             <FlatList 
                 data={mockData}
                 renderItem={({item, index}) => (<MessageBubble key={item.id} message={item.massage} owner={item.owner} owner_name={item.owner_name}/>)}
