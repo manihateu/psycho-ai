d5ff87dd34ae68178691d52d83fe62fd27542986
Nickita <102178609+manihateu@users.noreply.github.com>
2024-11-26 18:57:07 +0300

    navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ChatScreen } from './screens/ChatScreen/ChatScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View className="" style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name='ChatScreen'
+          component={ChatScreen}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/ChatScreen/ChatScreen.tsx
@@ -0,0 +1,5 @@
+export const ChatScreen = () => {
+    return (
+        <></>
+    )
+}
