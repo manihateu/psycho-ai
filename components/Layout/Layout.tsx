f7745db59efceb1fea1df34f1ba96dd8756722c0
Nickita <102178609+manihateu@users.noreply.github.com>
2024-11-30 02:26:42 +0300

    fixes

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -19,31 +19,31 @@ const Layout = ({children, name, canBack} : TLayoutProps) => {
   return (
     <ComSafeAreaView style={{backgroundColor: 'White'}}>
         <View className='w-full flex p-3 flex-row gap-x-[8px] items-center justify-center'>
-            <Text className='font-Comfortaa '>Ментальный</Text>
+            <Text className='font-Comfortaa min-w-[75]'>Ментальный</Text>
             <Logo width={30} height={30}/>
-            <Text className='font-Comfortaa '>Помощник</Text>
+            <Text className='font-Comfortaa min-w-[75px]'>Помощник</Text>
         </View>
         <View className='flex-1 bg-white'>
             {children}
         </View>
         <View className='bg-white flex flex-row py-3 justify-around'>
-            <View className='flex items-center gap-y-[8px]'>
+            <View className='flex items-center gap-y-[8px] min-w-[70px]'>
                 <Home width={30} heigth={30}/>
-                <Text className='font-Comfortaa text-[#A0A3B1]'>Главная</Text>
+                <Text className='font-Comfortaa text-[#A0A3B1] '>Главная</Text>
             </View>
-            <View className='flex items-center gap-y-[8px]'>
+            <View className='flex items-center gap-y-[8px] min-w-[70px]'>
                 <Loon width={30} heigth={30}/>
                 <Text className='font-Comfortaa text-[#A0A3B1]'>Сон</Text>
             </View>
-            <View className='flex items-center gap-y-[8px]'>
+            <View className='flex items-center gap-y-[8px] min-w-[70px]'>
                 <LogoBtn width={30} heigth={30}/>
                 <Text className='font-Comfortaa text-[#A0A3B1]'>Помощник</Text>
             </View>
-            <View className='flex items-center gap-y-[8px]'>
+            <View className='flex items-center gap-y-[8px] min-w-[70px]'>
                 <Music width={30} heigth={30}/>
                 <Text className='font-Comfortaa text-[#A0A3B1]'>Музыка</Text>
             </View>
-            <View className='flex items-center gap-y-[8px]'>
+            <View className='flex items-center gap-y-[8px] min-w-[70px]'>
                 <Profile width={30} heigth={30}/>
                 <Text className='font-Comfortaa text-[#A0A3B1]'>Профиль</Text>
             </View>
