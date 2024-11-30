48c73dc19b82b028a10dd4a64f6d17641f3ca3d0
Nikolaych228 <143743185+Nikolaych228@users.noreply.github.com>
2024-12-01 02:25:05 +0400

    OnboardingScreen

--- screens/OnboardingScreen/OnboardingScreen.tsx
+++ screens/OnboardingScreen/OnboardingScreen.tsx
@@ -1,22 +1,23 @@
 import React, { Children } from 'react'
-import { Image, ImageBackground, Text } from 'react-native'
+import { Button, Image, ImageBackground, Pressable, StatusBar, Text } from 'react-native'
 import { View } from 'react-native'
 import ComSafeAreaView from '../../shared/ComSafeAreaView/ComSafeAreaView'
 import OnboardingLogo from '../../assets/OnboardingLogo.png'
 import Logo from '../../assets/logo.svg'
 import OnboardingBg from '../../assets/OnboardingBg.png'
 import ComButton from '../../shared/ComButton/ComButton'
+import { ComAnimatedAppearance } from '../../shared/ComAnimatedAppearance/ComAnimatedAppearance'
 
 const OnboardingScreen = () => {
   return (
         <ComSafeAreaView style={{backgroundColor: '#fff'}}>
           <ImageBackground source={OnboardingBg} className='flex-1 h-3/5'>
             
-          <View className='w-full flex p-3 flex-row items-center mt-10 justify-center'>
+          <View className={`w-full flex p-3 flex-row items-center mt-[${StatusBar.currentHeight ? 50 - StatusBar.currentHeight : 30}px] justify-center mt-10`}>           
             <Text className='font-Comfortaa min-w-[102px] mr-[8px]'>Ментальный</Text>
             <Logo width={30} height={30} />
             <Text className='font-Comfortaa min-w-[102px] ml-[8px]'>Помощник</Text>
-            </View>
+          </View>
               <View className='flex items-center mt-16'>
                 <Image source={OnboardingLogo}/>
               </View>
@@ -24,17 +25,26 @@ const OnboardingScreen = () => {
                 <Text className='font-Comfortaa text-2xl'>
                   Начни работу с себя
                 </Text>
-                <Text className='mt-4 font-Comfortaa text-gray-400 text-base text-center'>
+              </View>
+              <View className='flex mt-4 mb-14'>
+                <Text className='w-full font-Comfortaa text-gray-400 text-base px-[18px] text-center'>
                   Создано для поддержки ментального здоровья работников РЖД
                 </Text>
               </View>
-              <View className='mt-14 w-full'>
-                <ComButton title='Зарегистрироваться' className='m-3 h-16'/>
-              </View>
-              <View className='flex items-center'>
-                <Text className='font-Comfortaa text-gray-400'>
+                
+              
+              <ComAnimatedAppearance className=' w-full'>
+                <ComButton title='Зарегистрироваться' className='mx-3 my-2 h-16'/>
+              </ComAnimatedAppearance>
+              <View className='flex flex-row w-full px-1 justify-center  '>
+                <Text className='font-Comfortaa text-gray-400 mr-2'>
                   Уже есть аккаунт?
                 </Text>
+                <Pressable >
+                  <Text className='font-Comfortaa text-blue-600'>
+                    Войти
+                  </Text>
+                </Pressable>
               </View>
             
           </ImageBackground>
