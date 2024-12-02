f4ef333729d20dde58c8a706b80d2c0b79ec8a33
Nickita <102178609+manihateu@users.noreply.github.com>
2024-12-03 01:06:36 +0300

    Update auth.service.ts

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
@@ -11,6 +11,10 @@ export class AuthService {
   ) {}
 
   async register(name: string, email: string, password: string, role: "USER" | "ADMIN") {
+    const searched_user = await this.usersService.findUserByEmail(email);
+    if (searched_user) {
+        throw new HttpException("пользователь с таким email уже есть", HttpStatus.BAD_REQUEST)
+    }
     const user = await this.usersService.createUser(name, email, password, role);
     return this.generateTokens(user.id, user.email, user.role);
   }
