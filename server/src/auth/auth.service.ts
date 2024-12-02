52bb8ef944e1dbc992d3a635e1231da03304908f
Nickita <102178609+manihateu@users.noreply.github.com>
2024-12-03 00:19:12 +0300

    authorization

---
+++ server/src/auth/auth.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async register(name: string, email: string, password: string) {
+    const user = await this.usersService.createUser(name, email, password);
+    return this.generateTokens(user.id, user.email);
+  }
+
+  async login(email: string, password: string) {
+    const user = await this.usersService.findUserByEmail(email);
+    if (!user) throw new UnauthorizedException('Invalid credentials');
+
+    const passwordMatches = await bcrypt.compare(password, user.password);
+    if (!passwordMatches) throw new UnauthorizedException('Invalid credentials');
+
+    return this.generateTokens(user.id, user.email);
+  }
+
+  generateTokens(userId: number, email: string) {
+    const payload = { sub: userId, email };
+    const accessToken = this.jwtService.sign(payload, {
+      secret: process.env.JWT_SECRET,
+      expiresIn: '15m',
+    });
+
+    const refreshToken = this.jwtService.sign(payload, {
+      secret: process.env.JWT_REFRESH_SECRET,
+      expiresIn: '7d',
+    });
+
+    return { accessToken, refreshToken };
+  }
+}
