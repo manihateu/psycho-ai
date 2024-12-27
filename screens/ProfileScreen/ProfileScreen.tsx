0f0a11c782b6cb82b3e3d4d2eb207968db67d80d
Nickita <102178609+manihateu@users.noreply.github.com>
2024-12-27 22:31:54 +0300

    Revert "add back button"
    This reverts commit db08168d562cf0c343141a42d67badbd3cff1c39.

--- screens/ProfileScreen/ProfileScreen.tsx
+++ screens/ProfileScreen/ProfileScreen.tsx
@@ -5,12 +5,10 @@ import Like from '../../assets/favorits.svg';
 import Listening from '../../assets/listening.svg';
 import Options from '../../assets/options.svg';
 import Quit from '../../assets/quit.svg';
-import { StackNavigation } from '../../components/RootNavigator/RootNavigator';
 import { mixins } from '../../constans';
 import { useQuit } from '../../shared/hooks/useQuit';
 import { useGetUserQuery } from '../../store/api/authorizeApiSlice';
 import { toast } from '@backpackapp-io/react-native-toast';
-import { useNavigation } from '@react-navigation/native';
 import {
     Dimensions,
     ImageBackground,
@@ -27,7 +25,6 @@ const ProfileScreen = () => {
         toast.error('Произошла ошибка');
     }
     const { quitHandler } = useQuit();
-    const navigation = useNavigation<StackNavigation>();
     return (
         <ScrollView className="py-3">
             <View className="w-full flex flex-row justify-around">
@@ -61,9 +58,6 @@ const ProfileScreen = () => {
             </View>
             <View className="w-full flex flex-row justify-around mt-3">
                 <TouchableOpacity
-                    onPress={() => {
-                        navigation.navigate('LikedScreen');
-                    }}
                     className="rounded-3xl p-3 shandow bg-white relative"
                     style={[
                         { minWidth: Dimensions.get('window').width / 2 - 16, minHeight: 150 },
