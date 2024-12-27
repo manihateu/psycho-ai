42ea933eeec71b531889faba692e9b66755d5958
Nickita <102178609+manihateu@users.noreply.github.com>
2024-12-27 23:32:11 +0300

    Update MusicScreen.tsx

--- screens/MusicScreen/MusicScreen.tsx
+++ screens/MusicScreen/MusicScreen.tsx
@@ -37,7 +37,7 @@ const MusicScreen = () => {
                 </TouchableOpacity>
             </ScrollView>
             <ScrollView  className="px-[14px] pt-[15px]">
-                <View className="flex flex-row justify-center w-full  mt-3">
+                <View className="flex flex-row justify-center w-full mb-3">
                     <ImageBackground source={{ uri: 'https://a.d-cd.net/10e9348s-960.jpg' }} imageStyle={{borderRadius: 16}} className="min-h-[200px]" style={{width: Dimensions.get('screen').width / 2 - 14}}>
                         <BlurView intensity={100}  className="mt-auto w-full p-3 rounded-b-2xl">
                             <Text className="font-ComfortaaBold">7 дней расслабления</Text>
@@ -49,7 +49,7 @@ const MusicScreen = () => {
                         </BlurView>
                     </ImageBackground>
                 </View>
-                <View className="flex flex-row justify-center w-full  mt-3">
+                <View className="flex flex-row justify-center w-full mb-3">
                     <ImageBackground source={{ uri: 'https://a.d-cd.net/10e9348s-960.jpg' }} imageStyle={{borderRadius: 16}} className="min-h-[200px]" style={{width: Dimensions.get('screen').width / 2 - 14}}>
                         <BlurView intensity={100}  className="mt-auto w-full p-3 rounded-b-2xl">
                             <Text className="font-ComfortaaBold">7 дней расслабления</Text>
@@ -61,7 +61,7 @@ const MusicScreen = () => {
                         </BlurView>
                     </ImageBackground>
                 </View>
-                <View className="flex flex-row justify-center w-full  mt-3">
+                <View className="flex flex-row justify-center w-full  mb-3">
                     <ImageBackground source={{ uri: 'https://a.d-cd.net/10e9348s-960.jpg' }} imageStyle={{borderRadius: 16}} className="min-h-[200px]" style={{width: Dimensions.get('screen').width / 2 - 14}}>
                         <BlurView intensity={100}  className="mt-auto w-full p-3 rounded-b-2xl">
                             <Text className="font-ComfortaaBold">7 дней расслабления</Text>
